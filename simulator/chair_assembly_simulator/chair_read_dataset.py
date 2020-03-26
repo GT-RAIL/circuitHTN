f195c05ffe16e0a5d66962f5924abf7d433262b9
Nithin-Shrivatsav96 <nithinshri@gatech.edu>
2020-03-26 18:34:45 -0400

    dataset_annotated_chair

---
+++ simulator/chair_assembly_simulator/chair_read_dataset.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+python function to parse the chair assembly dataset
+"""
+# -*- coding: utf-8 -*-
+"""
+file to parse the dataset files and extract the actions in correct order
+"""
+from os import listdir
+from os.path import isfile, join
+
+def build_dataset(dataset_folder, outputToFile):
+    action_dataset = []
+    for f in listdir(dataset_folder):
+        filename = join(dataset_folder, f)
+        if isfile(filename):
+            action_list = {}
+            action_timestamps = []
+            action = []
+            file = open(join(dataset_folder, f),"r")
+            for line in file.readlines():
+                word_list = line.split()
+#                if word_list[-1] == 'cut_and_mix_ingredients' or word_list[-1] == 'prepare_dressing':
+#                    continue
+#                elif word_list[-1].find('core') !=-1 :
+#                    action_list[word_list[0]] = word_list[-1]
+#                    action_timestamps.append(int(word_list[0]))
+#
+#        #    print(action_list)
+#            action_timestamps.sort()
+#            for t in action_timestamps:
+#                action.append(action_list[str(t)])
+#        #    print(action)
+#            action_dataset.append(action)
+#            file.close()
+#
+#            if outputToFile:
+#                output_file = open(join(dataset_output, f), "w+")
+#                for single_action in action:
+#                    output_file.write(single_action + "\n")
+#                output_file.close()
+#
+#    return action_dataset
+
+if __name__=="__main__":
+    dataset_folder = "./50_salad_dataset/ann-ts/activityAnnotations"
+    action_dataset = build_dataset(dataset_folder, True)
+    # print(action_dataset)
+    print(len(action_dataset))
+
+    ## knowing the actions in the dataset
+    unique_actions = set()
+    for demonstration in action_dataset:
+        for action in demonstration:
+            unique_actions.add(action)
+
+    print(unique_actions)
+
+
---
+++ simulator/chair_assembly_simulator/chair_simulator.py
@@ -0,0 +1,15 @@
+"""
+functions to simulate the chair construction task
+"""
+
+class ChairWorld(self):
+    def __init__(self):
+        self.chair_top = 0
+        self.left_side = 0
+        self.right_side = 0
+        self.back_leg = 0
+        self.front_leg = 0
+    
+        ## check what's in human hand
+        ## check what's in table
+        ## check what's in chair structure
